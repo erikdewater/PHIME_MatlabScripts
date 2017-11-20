2ed73a5b8d459ccc2640320a42c232cf0deab351
erikdewater <33238734+erikdewater@users.noreply.github.com>
2017-11-20 15:11:11 -0500

    Matlab scripts without fieldmap correction
    Matlab scripts for preprocessing and 1st level analyses of PHIME task-based fMRI data, without fieldmap correction

---
+++ preprocessing1stlevel_FT.m
@@ -0,0 +1,83 @@
+clear
+
+filepath = '/Users/dewate01/Desktop/Test_PHIME';
+inputs = cell(0, 1);
+test_script_Yuri_fMRI
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.tpm = {'/Users/dewate01/Downloads/spm12/tpm/TPM.nii'};
+%% File all the folders in the outer folder-(subjects)
+dirinfo = dir(filepath); %Find everything inside the folder
+dirinfo(~[dirinfo.isdir]) = []; %lists all subfolders in 'filepath' (Note: they should be in alphabetical order)
+dirinfo(1:2) = []; % remove any subfolders that you don't want to analyze
+
+%% Find all folders in the inner folders (assuming everything is the same
+%%across subjects)-(fMRI tasks)
+dirinfo_FMRI = dir(strcat(filepath,'/',dirinfo(1).name,'/fMRI')); %Find everything inside the subfolder
+dirinfo_FMRI(~[dirinfo_FMRI.isdir]) = []; %lists all subsubfolders in the subfolder
+dirinfo_FMRI(1:2) = []; % remove any subsubfolders that you don't want to use
+
+%% Find all folders in the iner folders (assuming everything is the same
+%%across subjects)-(structural)
+dirinfo_struct = dir(strcat(filepath,'/',dirinfo(1).name)); %Find everything inside the subfolder
+dirinfo_struct(~[dirinfo_struct.isdir]) = []; %lists all subsubfolders in the subfolder
+dirinfo_struct(1:2) = []; % remove any subsubfolders that you don't want to use
+
+%% Preallocate space
+FullFilePath_FMRI=strings(numel(dirinfo)*numel(dirinfo_FMRI),1);
+NewName_FMRI=strings(numel(dirinfo)*numel(dirinfo_FMRI),1);
+Command_FMRI=strings(numel(dirinfo)*numel(dirinfo_FMRI),1);
+
+FullFilePath=strings(numel(dirinfo)*numel(dirinfo_struct),1);
+NewName=strings(numel(dirinfo)*numel(dirinfo_struct),1);
+Command=strings(numel(dirinfo)*numel(dirinfo_struct),1);
+
+%% Create everything
+for h=1:numel(dirinfo) %Subjects loop
+    
+    for e=1:1%numel(dirinfo_struct) %structural
+        FullFilePath(numel(dirinfo_struct)*(h-1)+e)=strcat(filepath,'/',dirinfo(h).name,'/',dirinfo_struct(e).name, '/'); % Full file path
+        dirinfo3 = dir(char(FullFilePath(numel(dirinfo_struct)*(h-1)+e)));
+        dirinfo3(1:3) = [];
+        
+        for f=1:numel(dirinfo3)
+            matlabbatch1{2}.spm.util.import.dicom.data{f,1}=char(strcat(FullFilePath(numel(dirinfo_struct)*(h-1)+e),dirinfo3(f).name));
+        end
+        matlabbatch1{2}.spm.util.import.dicom.outdir{1,1} = char(FullFilePath(numel(dirinfo_struct)*(h-1)+e));
+    end
+    
+    for g=1:1%1:numel(dirinfo_FMRI) %Tasks loop
+        FullFilePath_FMRI(numel(dirinfo_FMRI)*(h-1)+g)=strcat(filepath,'/',dirinfo(h).name,'/fMRI/',dirinfo_FMRI(g).name, '/'); % Full file path
+        dirinfo3_FMRI = dir(char(FullFilePath_FMRI(numel(dirinfo_FMRI)*(h-1)+g)));
+        dirinfo3_FMRI(1:3) = [];
+        
+        for f=1:numel(dirinfo3_FMRI)
+            matlabbatch1{1}.spm.util.import.dicom.data{f,1}=char(strcat(FullFilePath_FMRI(numel(dirinfo_FMRI)*(h-1)+g),dirinfo3_FMRI(f).name));
+        end
+        matlabbatch1{1}.spm.util.import.dicom.outdir{1,1} = char(FullFilePath_FMRI(numel(dirinfo_FMRI)*(h-1)+g));
+    end
+     spm('defaults', 'FMRI');
+     spm_jobman('run', matlabbatch1, inputs{:});
+    
+end
+for h=1:numel(dirinfo) %Subjects loop
+    
+    for j=1:1%numel(dirinfo_FMRI) %Tasks loop 
+        FullFilePath_FMRI2=strcat(filepath,'/',dirinfo(h).name,'/fMRI/',dirinfo_FMRI(j).name, '/'); % Full file path
+        cd(char(FullFilePath_FMRI2));
+        prepr = dir('*swc*.nii'); 
+        prepr(1)=[];
+        rp = dir('*rp*.txt');
+        for f=1:numel(prepr)
+            matlabbatch2{1}.spm.stats.fmri_spec.sess.scans{f,1}=char(strcat(FullFilePath_FMRI2,prepr(f).name));    
+        end
+        matlabbatch2{1}.spm.stats.fmri_spec.sess.multi_reg{1,1}=char(strcat(FullFilePath_FMRI2,rp.name));
+        cd(char(strcat(filepath, '/', dirinfo(h).name)))
+        dir_model = strcat('model', '_', dirinfo(h).name, '_', dirinfo_FMRI(j).name);
+        mkdir(char(dir_model));
+        matlabbatch2{1}.spm.stats.fmri_spec.dir{1,1}= char(strcat(filepath, '/', dirinfo(h).name, '/', dir_model,'/'));
+        matlabbatch2{2}.spm.stats.fmri_est.spmmat{1,1} = char(strcat(filepath,'/',dirinfo(h).name,'/',dir_model, '/','SPM.mat'));
+        matlabbatch2{3}.spm.stats.con.spmmat{1,1} = char(strcat(filepath,'/',dirinfo(h).name,'/',dir_model, '/','SPM.mat'));
+
+    end
+     spm('defaults', 'FMRI');
+     spm_jobman('run', matlabbatch2, inputs{:});
+end
---
+++ test_script_Yuri_fMRI_NBack.m
@@ -0,0 +1,124 @@
+
+matlabbatch1{1}.spm.util.import.dicom.root = 'flat';
+matlabbatch1{1}.spm.util.import.dicom.protfilter = '.*';
+matlabbatch1{1}.spm.util.import.dicom.convopts.format = 'nii';
+matlabbatch1{1}.spm.util.import.dicom.convopts.icedims = 0;
+
+matlabbatch1{2}.spm.util.import.dicom.root = 'flat';
+matlabbatch1{2}.spm.util.import.dicom.protfilter = '.*';
+matlabbatch1{2}.spm.util.import.dicom.convopts.format = 'nii';
+matlabbatch1{2}.spm.util.import.dicom.convopts.icedims = 0;
+
+matlabbatch1{3}.spm.temporal.st.scans{1}(1) = cfg_dep('DICOM Import: Converted Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch1{3}.spm.temporal.st.nslices = 70;
+matlabbatch1{3}.spm.temporal.st.tr = 1;
+matlabbatch1{3}.spm.temporal.st.ta = 0.985714285714286;
+matlabbatch1{3}.spm.temporal.st.so = [685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195 685 0 490 97.5 587.5 292.5 782.5 390 880 195];
+matlabbatch1{3}.spm.temporal.st.refslice = 490;
+matlabbatch1{3}.spm.temporal.st.prefix = 'a';
+
+matlabbatch1{4}.spm.spatial.realign.estwrite.data{1}(1) = cfg_dep('Slice Timing: Slice Timing Corr. Images (Sess 1)', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch1{4}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch1{4}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+matlabbatch1{4}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch1{4}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch1{4}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch1{4}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+
+matlabbatch1{5}.spm.spatial.coreg.estwrite.ref(1) = cfg_dep('DICOM Import: Converted Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch1{5}.spm.spatial.coreg.estwrite.source(1) = cfg_dep('Realign: Estimate & Reslice: Mean Image', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rmean'));
+matlabbatch1{5}.spm.spatial.coreg.estwrite.other(1) = cfg_dep('Realign: Estimate & Reslice: Resliced Images (Sess 1)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{1}, '.','rfiles'));
+matlabbatch1{5}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch1{5}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch1{5}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch1{5}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch1{5}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch1{5}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch1{5}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch1{5}.spm.spatial.coreg.estwrite.roptions.prefix = 'c';
+
+matlabbatch1{6}.spm.spatial.normalise.estwrite.subj.vol(1) = cfg_dep('DICOM Import: Converted Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch1{6}.spm.spatial.normalise.estwrite.subj.resample(1) = cfg_dep('Coregister: Estimate & Reslice: Resliced Images', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rfiles'));
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+matlabbatch1{6}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+matlabbatch1{6}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+                                                               78 76 85];
+matlabbatch1{6}.spm.spatial.normalise.estwrite.woptions.vox = [1 1 1];
+matlabbatch1{6}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+matlabbatch1{6}.spm.spatial.normalise.estwrite.woptions.prefix = 'w';
+
+matlabbatch1{7}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Estimate & Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{6}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch1{7}.spm.spatial.smooth.fwhm = [5 5 5];
+matlabbatch1{7}.spm.spatial.smooth.dtype = 0;
+matlabbatch1{7}.spm.spatial.smooth.prefix = 's';
+
+matlabbatch2{1}.spm.stats.fmri_spec.timing.units = 'secs';
+matlabbatch2{1}.spm.stats.fmri_spec.timing.RT = 1;
+matlabbatch2{1}.spm.stats.fmri_spec.timing.fmri_t = 70;
+matlabbatch2{1}.spm.stats.fmri_spec.timing.fmri_t0 = 35;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.scans = {};
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).name = 'Rest';
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).onset = [0
+                                                         60
+                                                         120
+                                                         180
+                                                         240
+                                                         300
+                                                         360
+                                                         420];
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).duration = 20;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).tmod = 0;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(1).orth = 1;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).name = '2Back';
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).onset = [20
+                                                         140
+                                                         260
+                                                         380];
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).duration = 40;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).tmod = 0;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(2).orth = 1;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).name = '3Back';
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).onset = [80
+                                                         200
+                                                         320
+                                                         440];
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).duration = 40;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).tmod = 0;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch2{1}.spm.stats.fmri_spec.sess.cond(3).orth = 1;
+matlabbatch2{1}.spm.stats.fmri_spec.sess.multi = {''};
+matlabbatch2{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
+matlabbatch2{1}.spm.stats.fmri_spec.sess.hpf = 128;
+matlabbatch2{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch2{1}.spm.stats.fmri_spec.bases.hrf.derivs = [1 0];
+matlabbatch2{1}.spm.stats.fmri_spec.volt = 1;
+matlabbatch2{1}.spm.stats.fmri_spec.global = 'None';
+matlabbatch2{1}.spm.stats.fmri_spec.mthresh = 0.8;
+matlabbatch2{1}.spm.stats.fmri_spec.mask = {''};
+matlabbatch2{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
+
+matlabbatch2{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch2{2}.spm.stats.fmri_est.method.Classical = 1;
+
+matlabbatch2{3}.spm.stats.con.consess{1}.tcon.name = 'Nback>rest';
+matlabbatch2{3}.spm.stats.con.consess{1}.tcon.weights = [-2 0 1 0 1 0 0 0 0 0 0 0 0];
+matlabbatch2{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch2{3}.spm.stats.con.consess{2}.tcon.name = 'rest>Nback';
+matlabbatch2{3}.spm.stats.con.consess{2}.tcon.weights = [2 0 -1 0 -1 0 0 0 0 0 0 0 0];
+matlabbatch2{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch2{3}.spm.stats.con.consess{3}.tcon.name = '3Back>2Back';
+matlabbatch2{3}.spm.stats.con.consess{3}.tcon.weights = [0 0 -1 0 1 0 0 0 0 0 0 0 0];
+matlabbatch2{3}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch2{3}.spm.stats.con.delete = 1;
